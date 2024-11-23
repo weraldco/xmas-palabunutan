4b6ed825ca758d385c97083206693f0b7a851e2e
Werald Opolento <werald.opolento@gmail.com>
2024-11-23 12:07:50 +0800

    test

--- action/auth.ts
+++ action/auth.ts
@@ -29,24 +29,22 @@ export const getUserByEmail = async (email: string) => {
 };
 
 export const loginWithCreds = async (formData: FormData) => {
-	const rawFormData = {
-		email: formData.get('email'),
-		password: formData.get('password'),
-		role: 'USER',
-		redirectTo: '/',
-	};
-
+	// const rawFormData = {
+	// 	email: formData.get('email'),
+	// 	password: formData.get('password'),
+	// 	role: 'USER',
+	// 	redirectTo: '/',
+	// };
 	const existingUser = await getUserByEmail(formData.get('email') as string);
 	console.log(existingUser);
-
-	try {
-		await signIn('credentials', rawFormData);
-	} catch (error) {
-		console.log(error);
-	} finally {
-		revalidatePath('/dashboard');
-		redirect('/dashboard');
-	}
+	// try {
+	// 	await signIn('credentials', rawFormData);
+	// } catch (error) {
+	// 	console.log(error);
+	// } finally {
+	// 	revalidatePath('/dashboard');
+	// 	redirect('/dashboard');
+	// }
 };
 
 export const registerNewUser = async (formData: FormData) => {
