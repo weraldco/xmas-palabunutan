c17150b884e6e1dfc2cb6803216c8b436bad7e4a
Werald Opolento <werald.opolento@gmail.com>
2024-11-23 17:44:11 +0800

    testing 1

--- action/auth.ts
+++ action/auth.ts
@@ -28,15 +28,18 @@ export const getUserByEmail = async (email: string) => {
 	}
 };
 
-export const loginWithCreds = async (formData: FormData) => {
+export const loginWithCreds = async () => {
 	// const rawFormData = {
 	// 	email: formData.get('email'),
 	// 	password: formData.get('password'),
 	// 	role: 'USER',
 	// 	redirectTo: '/',
 	// };
-	const existingUser = await getUserByEmail(formData.get('email') as string);
-	console.log(existingUser);
+	console.log('Logging in...');
+	const allUser = await db.user.findMany();
+	return allUser;
+	// const existingUser = await getUserByEmail(formData.get('email') as string);
+	// console.log(existingUser);
 	// try {
 	// 	await signIn('credentials', rawFormData);
 	// } catch (error) {
--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { loginWithCreds } from '@/action/auth';
 // import { checkEmail } from '@/utils/helper';
+import db from '@/db';
 import Link from 'next/link';
 import React from 'react';
 import AuthButtn from './AuthButtn';
@@ -10,27 +11,19 @@ const LoginForm = () => {
 	// const [password, setPassword] = useState('');
 	// const [error, setError] = useState('');
 
-	// const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-	// 	setError('');
-	// 	e.preventDefault();
-	// 	if (!email || !password) {
-	// 		setError('All fields are required');
-	// 	} else {
-	// 		const form = new FormData();
-	// 		form.append('email', email);
-	// 		form.append('password', password);
-
-	// 		loginWithCreds(form);
-	// 	}
-	// };
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const getUsers = await loginWithCreds();
+		console.log(getUsers);
+	};
 	return (
 		<div>
 			<div className="flex justify-center items-center">
 				<div className="min-w-[400px] grid gap-4">
 					<h1 className="text-2xl font-bold">Login Page</h1>
 					<form
-						// onSubmit={handleSubmit}
-						action={loginWithCreds}
+						onSubmit={handleSubmit}
+						// action={loginWithCreds}
 						className="flex flex-col gap-4"
 					>
 						<div className="flex flex-col gap-1">
